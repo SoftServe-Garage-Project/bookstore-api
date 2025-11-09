45ad9b60ad2e80639a6d483ae79bf6c6da4f0f41
Максим Назаренко <maksimnazarenko779@gmail.com>
2025-11-09 12:56:59 +0200

    AccountService minor changes

--- src/main/java/com/softserve/bookstoreapi/service/impl/AccountService.java
+++ src/main/java/com/softserve/bookstoreapi/service/impl/AccountService.java
@@ -3,12 +3,11 @@ package com.softserve.bookstoreapi.service.impl;
 import com.softserve.bookstoreapi.dto.UserRegisterRequestDTO;
 import com.softserve.bookstoreapi.dto.UserRegisterResponseDTO;
 import com.softserve.bookstoreapi.exception.EmailAlreadyExistsException;
-import com.softserve.bookstoreapi.exception.PasswordMismatchException;
-import com.softserve.bookstoreapi.logger.LoggerUtils;
 import com.softserve.bookstoreapi.model.Account;
 import com.softserve.bookstoreapi.model.enums.UserRole;
-import com.softserve.bookstoreapi.repository.AccountRepository;
+import com.softserve.bookstoreapi.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -16,20 +15,19 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 
+import static com.softserve.bookstoreapi.logger.LoggerUtils.obfuscate;
+
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class AccountService {
-    private final AccountRepository accountRepository;
+    private final UserRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder;
-    private final LoggerUtils loggerUtils;
     private final ModelMapper mapper;
 
     @Transactional
     public UserRegisterResponseDTO registerUser(UserRegisterRequestDTO requestDTO) {
-        if (!requestDTO.getPassword().equals(requestDTO.getConfirmPassword())) {
-            throw new PasswordMismatchException("error.password.mismatch", requestDTO.getEmail());
-        }
-        if (accountRepository.existsByEmail(requestDTO.getEmail())) {
+        if (userRepository.existsByEmail(requestDTO.getEmail())) {
             throw new EmailAlreadyExistsException("error.email.already.exists", requestDTO.getEmail());
         }
 
@@ -40,8 +38,8 @@ public class AccountService {
         account.setRole(UserRole.ROLE_CUSTOMER);
         account.setBalance(BigDecimal.ZERO);
 
-        Account savedAccount = accountRepository.save(account);
-        loggerUtils.logRegistrationSuccess(requestDTO.getEmail());
+        Account savedAccount = userRepository.save(account);
+        log.info("Successful registration for email: {}", obfuscate(requestDTO.getEmail()));
         return mapper.map(savedAccount, UserRegisterResponseDTO.class);
     }
 }
