b7035cc6bac05b256f77293fa2b6eff761634e77
KochubeynikDima <kochubeynikdima@gmail.com>
2026-02-09 15:32:46 +0200

    Books are no longer duplicated if they have the same parameters

--- src/main/java/com/softserve/bookstoreapi/repository/BookRepository.java
+++ src/main/java/com/softserve/bookstoreapi/repository/BookRepository.java
@@ -2,6 +2,7 @@ package com.softserve.bookstoreapi.repository;
 
 import com.softserve.bookstoreapi.model.Author;
 import com.softserve.bookstoreapi.model.Book;
+import com.softserve.bookstoreapi.model.Language;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,6 +10,8 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -21,5 +24,7 @@ public interface BookRepository extends JpaRepository<Book, Long> {
     Page<Book> findByTitleContainingIgnoreCase(String title, Pageable pageable);
 
     Page<Book> findByTitleContainingIgnoreCaseAndGenreId(String title, Long genreId, Pageable pageable);
+
+    List<Book> findByTitleIgnoreCaseAndPublishedYearAndLanguage(String title, Integer publishedYear, Language language);
 }
 
